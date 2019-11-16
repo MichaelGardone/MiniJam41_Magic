7c51e57e50a36a24c8b164e265eee27d290561b2
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2019-11-16 15:14:49 -0700

    Full player anim test

--- TheReleastWizard/Assets/Resources/JasonModelCrap/PlayerAnimation.cs
+++ TheReleastWizard/Assets/Resources/JasonModelCrap/PlayerAnimation.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class PlayerAnimation : MonoBehaviour
 {
     private Animator animator;
-    
-    
+    private int isWalking;
+    private int isRunning;
+    private int isCasting;
+    private int isFalling;
+
+
+
+    public bool IsWalking;
+    public bool IsRunning;
+    public bool IsCasting;
+    public bool IsFalling;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        isWalking = Animator.StringToHash("isWalking");
+        isRunning = Animator.StringToHash("isRunning");
+        isCasting = Animator.StringToHash("isCasting");
+        isFalling = Animator.StringToHash("isFalling");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        animator.SetBool(isWalking, IsWalking);
+        animator.SetBool(isRunning, IsRunning);
+        animator.SetBool(isFalling, IsFalling);
+        animator.SetBool(isCasting, IsCasting);
     }
 }
