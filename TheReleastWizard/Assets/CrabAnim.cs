7560ecedc93412c9a1fbd5686e2eee43a2baa604
jjcosmos <32972128+jjcosmos@users.noreply.github.com>
2019-11-16 20:50:48 -0700

    Crabbo animations in

---
+++ TheReleastWizard/Assets/CrabAnim.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrabAnim : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+    private Animator animator;
+    private int State; //0 = idle, 1= walking, 2= attacking
+
+    [Range(0, 2)]
+    public int PublicState;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        State = Animator.StringToHash("AnimState");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetInteger(State, PublicState);
+    }
+}
